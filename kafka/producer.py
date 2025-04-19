38a314cb56330bed0bb5e4f8427d1c3d7244207d
JasmiinaHK <jasmina.hasanovic@stu.ibu.edu.ba>
2025-04-19 23:11:59 +0200

    Update producer.py

--- kafka/producer.py
+++ kafka/producer.py
@@ -3,19 +3,19 @@ import os
 import json
 import time
 
-# Inicijalizacija Kafka producer-a
+# Kafka producer initialization
 producer = KafkaProducer(
     bootstrap_servers='localhost:9092',
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
-# Folder sa slikama
+# Folder with images
 image_folder = "images_subset"
 
 # Kafka topic
 topic_name = "image_paths"
 
-# Slanje slike po slike
+# Sending images one by one
 for filename in os.listdir(image_folder):
     if filename.lower().endswith((".jpg", ".jpeg", ".png")):
         image_path = os.path.join(image_folder, filename)
